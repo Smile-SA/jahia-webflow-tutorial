1b7dd505f1b44830d8d5c12cd129a82e1f505800
Fabien RASSINIER <fabien.rassinier@smile.fr>
2015-10-19 00:13:03 +0200

    Add JavaDoc

--- src/main/java/fr/smile/jahia/handler/ContactInfoHandler.java
+++ src/main/java/fr/smile/jahia/handler/ContactInfoHandler.java
@@ -6,15 +6,31 @@ import org.jahia.services.content.JCRNodeWrapper;
 import org.jahia.services.content.JCRSessionWrapper;
 import org.jahia.services.content.JCRTemplate;
 import org.jahia.services.content.decorator.JCRUserNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.jcr.RepositoryException;
 import java.io.Serializable;
 
+/**
+ * Webflow handler in order to update with user contact information
+ */
 public class ContactInfoHandler implements Serializable {
 
+    private static final Logger logger = LoggerFactory.getLogger(ContactInfoHandler.class);
+
+    /**
+     * Update current user contact information
+     *
+     * @param contactInfo Java bean where temproray contact information are saved
+     * @param formNode    current resource
+     * @throws RepositoryException
+     */
     public void update(
             final ContactInfo contactInfo,
             final JCRNodeWrapper formNode) throws RepositoryException {
+        logger.debug("contactInfo=" + contactInfo.toString());
+        logger.debug("formNode=" + formNode.getPropertiesAsString());
         JCRTemplate.getInstance().doExecuteWithSystemSessionAsUser(
                 formNode.getSession().getUser(),
                 formNode.getSession().getWorkspace().getName(),
@@ -22,9 +38,15 @@ public class ContactInfoHandler implements Serializable {
                 new JCRCallback<Boolean>() {
                     @Override
                     public Boolean doInJCR(JCRSessionWrapper session) throws RepositoryException {
+                        logger.debug("user=" + formNode.getSession().getUser().getUsername());
+                        logger.debug("workspaceName=" + formNode.getSession().getWorkspace().getName());
+                        logger.debug("locale=" + formNode.getSession().getLocale());
+
                         final JCRUserNode userNode = session.getUserNode();
                         userNode.setProperty("j:email", contactInfo.getEmail());
                         session.save();
+                        logger.debug("JCR Session was saved!");
+
                         return Boolean.TRUE;
                     }
 
